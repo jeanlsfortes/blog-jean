657dafc826b681756ba5716e4fd3925005466f3c
jeanlsfortes <jeanlucaseh10@gmail.com>
2024-08-15 15:53:42 -0400

    Updates

---
+++ js/383.aa5209d6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkblog_jean"]=self["webpackChunkblog_jean"]||[]).push([[383],{7383:function(n,e,t){t.r(e),t.d(e,{default:function(){return g}});var s=t(6768);const u={id:"settings-page"},a=(0,s.Lk)("h1",null,"Settings",-1),r=(0,s.Lk)("p",null,"This is the sett page",-1),i=[a,r];function l(n,e){return(0,s.uX)(),(0,s.CE)("main",u,i)}var c=t(1241);const o={},f=(0,c.A)(o,[["render",l]]);var g=f}}]);
+//# sourceMappingURL=383.aa5209d6.js.map
